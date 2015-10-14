51b9774e5fb315533954c329c56b97bd82348b6c
Charlie Su <dragonsmirth@gmail.com>
2015-10-14 00:01:54 -0400

    Actual inital commit lol. Set up the layout for the first activity

---
+++ app/src/main/java/org/pieee/rovwiki/ieeerovdebugger2015/MainActivity.java
@@ -0,0 +1,13 @@
+package org.pieee.rovwiki.ieeerovdebugger2015;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);//draws the GUI on screen based on the contents of the XML file activity_main
+    }
+}
